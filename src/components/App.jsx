c607960ff91e6a25609e2b8aac1ebd8551cf46d4
ukrwebprom <ukrwebprom@gmail.com>
2023-03-11 19:50:40 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Base />}>
+      <Route path="goit-react-hw-05-movies/" element={<Base />}>
         <Route index element={<Trending />} />
         <Route path="/movies/:id" element={<MovieDetails />}>
           <Route path="reviews" element={<Reviews />} />
